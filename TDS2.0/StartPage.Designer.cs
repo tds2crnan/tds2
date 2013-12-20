7571e4f3c1fb616448b7fd9eecdb93f3ea1d7e70
tds2crnan <tds2crnan@yopmail.com>
2013-12-20 03:15:04 +0100

    test2

--- TDS2.0/StartPage.Designer.cs
+++ TDS2.0/StartPage.Designer.cs
@@ -32,15 +32,20 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttoniCal = new System.Windows.Forms.Button();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonImpHebdo
@@ -52,6 +57,7 @@
             this.buttonImpHebdo.TabIndex = 0;
             this.buttonImpHebdo.Text = "Hebdo";
             this.buttonImpHebdo.UseVisualStyleBackColor = true;
+            this.buttonImpHebdo.Click += new System.EventHandler(this.buttonImpHebdo_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -87,14 +93,16 @@
             this.label1.Text = "Consultation";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button2
+            // buttoniCal
             // 
-            this.button2.Location = new System.Drawing.Point(463, 505);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Jour";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttoniCal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttoniCal.Location = new System.Drawing.Point(3, 54);
+            this.buttoniCal.Name = "buttoniCal";
+            this.buttoniCal.Size = new System.Drawing.Size(194, 105);
+            this.buttoniCal.TabIndex = 0;
+            this.buttoniCal.Text = "Exporter en iCal";
+            this.buttoniCal.UseVisualStyleBackColor = true;
+            this.buttoniCal.Click += new System.EventHandler(this.buttoniCal_Click);
             // 
             // tableLayoutPanel2
             // 
@@ -144,12 +152,46 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.panel3, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttoniCal, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(573, 185);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 31.85484F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 68.14516F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(200, 162);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightCyan;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(194, 45);
+            this.panel3.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 45);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Exportation";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // StartPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel3);
             this.Controls.Add(this.tableLayoutPanel2);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "StartPage";
             this.Size = new System.Drawing.Size(1346, 618);
@@ -157,6 +199,8 @@
             this.panel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -167,10 +211,13 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttoniCal;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
     }
 }
--- TDS2.0/StartPage.cs
+++ TDS2.0/StartPage.cs
@@ -6,6 +6,10 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DDay.iCal;
+using DDay.iCal.Serialization.iCalendar;
+
+
 
 namespace Core
 {
@@ -20,6 +24,88 @@ namespace Core
         {
             Form dialog = new FormImpressionAnnuel();
             dialog.ShowDialog();
+        }
+
+        private void buttonImpHebdo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttoniCal_Click(object sender, EventArgs e)
+        {
+            //je fixe le premier jour de l'année
+            DateTime premier_jour = new DateTime(2013, 1, 1);
+
+
+            // je créé le dictionnaire des vacs pour l'agent donné et l'année donnée
+
+
+            List<IVacation> liste = DaoIVacation.findVacAnnee<IVacation>(60, premier_jour); // 60 = id de toto2
+            Dictionary<DateTime, IVacation> dico = new Dictionary<DateTime, IVacation>();
+            foreach (IVacation vacation in liste)
+            {
+                dico[vacation.Date] = vacation;
+            }
+
+            // Create a new calendar
+            IICalendar iCal = new iCalendar();
+
+            // Add the local time zone to the calendar
+            ITimeZone local = iCal.AddLocalTimeZone();
+
+
+
+            // boucle de douze mois
+            int i = 0;
+            for (i = 0; i < 12; i++)
+            {
+                // boucle pour chaque jour du mois concerné
+                int nbDays = DateTime.DaysInMonth(2013, i + 1);
+
+                int j = 0;
+                for (j = 0; j < nbDays; j++)
+                {
+
+                    //j'écris dans un fichier .ics
+                    if (dico.ContainsKey(premier_jour) == true)
+                    {
+                        string chaine_vac = dico[premier_jour].Type.makePrint();
+
+                        // Create a new event in the calendar
+                        // that uses our local time zone
+                        IEvent evt = iCal.Create<Event>();
+                        evt.Summary = chaine_vac;
+                        evt.Start = new iCalDateTime(premier_jour);
+                        evt.End = new iCalDateTime(premier_jour.AddHours(12));
+                        //evt.IsAllDay = true;
+                        evt.Location = "Athis-Mons CRNA-N 1609";
+                        //evt.Start = iCalDateTime.Today.AddHours(8).SetTimeZone(local);
+                        //evt.Duration = TimeSpan.FromHours(12);
+                    }
+
+                    //j'incrémente le jour
+                    premier_jour = premier_jour.AddDays(1);
+
+                }
+            }
+
+            // je récupère le iCal pour l'exporter en .ics
+
+            if (iCal.Events.Count > 0)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.DefaultExt = ".ics";
+                saveFileDialog1.FileName = DateTime.Now.ToString("yyyy_MM_dd") + "_AgendaMO";
+                saveFileDialog1.Filter = "iCal Calendar (ICS)|*.ics";
+                saveFileDialog1.Title = "Exporter son calendrier personnel";
+                DialogResult result = saveFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    string resultat = new iCalendarSerializer().SerializeToString(iCal);
+                    System.IO.File.WriteAllText(@saveFileDialog1.FileName, resultat);
+                    MessageBox.Show("Exportation de " + iCal.Events.Count + " évènements Ok");
+                }   
+            }
         }
     }
 }
